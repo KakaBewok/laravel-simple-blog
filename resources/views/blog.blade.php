8b2a482061d91281fb01e21968b8d35caa5de541
noprizal <noprizal@voltras.co.id>
2024-08-01 14:35:27 +0700

    init

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Blog</h1>
+    <article>
+        <h3>Title 1</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non labore nihil dolore nesciunt natus illo iusto
+            quidem, animi nemo cumque hic repudiandae distinctio!</p>
+    </article>
+
+    <article>
+        <h3>Title 2</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non labore nihil dolore nesciunt natus illo iusto
+            quidem, animi nemo cumque hic repudiandae distinctio!</p>
+    </article>
+</body>
+
+</html>

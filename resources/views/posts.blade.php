f04e296209669fdcb7f73825f7cff8b6e5d8b516
noprizal <noprizal@voltras.co.id>
2024-08-07 14:55:35 +0700

    add articles by

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,17 @@
 <x-layout>
     <x-slot:title>{{ $title }}</x-slot>
 
-    @foreach ($posts as $post)
+        @foreach ($posts as $post)
         <article class="max-w-screen-md py-8 border-b border-gray-300">
             <a href="/post/{{ $post->slug }}" class="hover:underline">
                 <h2 class="mb-1 text-3xl font-bold tracking-tight text-gray-900">{{ $post->title }}</h2>
             </a>
-            <div class="text-base text-gray-500"><a href="#">{{ $post->author->name }}</a> |
-                {{ $post->created_at->diffForHumans() }}</div>
+            <div class="text-base text-gray-500"><a href="/author/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> |
+                {{ $post->created_at->diffForHumans() }}
+            </div>
             <p class="my-4 font-light">{{ Str::limit($post->body, 120) }}</p>
             <a href="/post/{{ $post->slug }}" class="font-medium text-blue-600 hover:underline">Read more &raquo;</a>
         </article>
-    @endforeach
+        @endforeach
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -25,5 +27,13 @@ Route::get('/post/{post:slug}', function (Post $post) {
     return view('post', [
         'title' => 'Single Post',
         'post' => $post
+    ]);
+});
+
+Route::get('/author/{user}', function (User $user) {
+
+    return view('posts', [
+        'title' => 'Articles by ' . $user->name,
+        'posts' => $user->posts
     ]);
 });
